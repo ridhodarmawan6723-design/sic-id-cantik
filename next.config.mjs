213ac65b00927b0e44103f623f2b078570c37ea4
ridhodarmawan6723-design <ridhodarmawan6723@gmail.com>
2026-09-07 16:17:24 +0800

    Fix globals.css

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Tidak perlu matikan SWC karena Next 13 pakai Webpack default
 };
+
 export default nextConfig;
